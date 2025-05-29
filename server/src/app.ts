281d62c3a2760adc23d26106c8fe2a582ba11c28
waheed.ahmed <waheed.ahmed@hq.bill.com>
2025-05-29 20:36:39 +0500

    resolving railway cors issue

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
   app.use(cors({
-    origin: ["http://localhost:5173", process.env.CLIENT_URL!],
+    origin: ["http://localhost:5173", "https://proxy-logger-tau.vercel.app/", process.env.CLIENT_URL!],
     credentials: true,
   }));
 
